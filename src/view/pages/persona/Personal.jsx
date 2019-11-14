a0688caf6e75e3c3a45adc713c14a717c4817f1a
Hernan <hernanrojasutani@gmail.com>
2019-11-14 15:42:47 -0500

    Add new files

---
+++ src/view/pages/persona/Personal.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+class Personal extends React.Component{
+    constructor(prop){
+        super(prop)
+
+        this.state={
+            list_personal:[],
+            personal:{},
+
+        }
+
+
+
+    }
+
+
+    
+
+
+    render (){
+        return (
+            <>
+                <h1>Hello React</h1>
+            </>
+        );
+    }
+
+}
+
+export default Personal;
---
+++ src/view/pages/solicitudes/Solicitud.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+class Solicitud extends React.Component{
+    constructor(prop){
+        super(prop);
+
+        this.state={
+            solicitudes:{
+
+            },
+            list_solicitudes:[],
+
+
+        }
+
+
+        
+
+    }
+
+}
+
+export default Solicitud;
