bd1c56db2f0772262a404609724222d4e893d787
Hernan <hernanrojasutani@gmail.com>
2019-11-19 11:57:08 -0500

    Add

--- src/view/pages/persona/Personal.jsx
+++ src/view/pages/persona/Personal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { server,api_name } from '../../../api/general';
 // const fetch = require('node-fetch');
+import axios from 'axios';
 
 class Personal extends React.Component{
     constructor(prop){
@@ -11,31 +12,40 @@ class Personal extends React.Component{
             personal:{},
 
         }
-
+                
         
-
-        
-        fetch('http://jsonplaceholder.typicode.com/todos')
-        .then(res => res.json())
-        .then(json =>{
-            console.log(json);
-        });
-
         this.cargarData();
     }
-
+    
     cargarData =()=>{
-        fetch(server+api_name+'/getusers')
-        .then(res => res.json())
-        .then(json =>{
-            console.log(json);
+        axios.get(server+api_name+'/getusers')
+        .then(res => {
+            console.log(res);
+        })
+        .catch(error => {
+            // alert(error)
+            console.log(error)
         });
-    }
+        
+        // fetch(server+api_name+'/getusers')
+        // .then(res => res.json())
+        // .then(json =>{
+        //     console.log(json);
+        // });
 
+        // axios.get("http://jsonplaceholder.typicode.com/todos")
+        // .then(res => {
+        //     console.log(res);
+        // })
+        // .catch(error => {
+        //     alert(error)
+        // });
+    }
+    
     render (){
         return (
             <>
-                <h1>Personal  Redux </h1>
+                {/* <h1>Personal  Redux </h1> */}
             </>
         );
     }
